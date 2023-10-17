ec4230e56377e520a6036bfd4e2f5e7e118148dc
ⵟⴰⵕⵉⴽ ⴱⴰⴽⵉⵕ <41262131+TRKBKR@users.noreply.github.com>
2023-10-17 20:34:15 +0100

    Create a.js
    testing cors

---
+++ a.js
@@ -0,0 +1,11 @@
+alert("t4r1k-b41");
+prompt("t4r1k-b41");
+console.log("t4r1k-b41");
+
+alert(document.domain);
+prompt(document.domain);
+console.log(document.domain);
+
+alert(document.cookie);
+prompt(document.cookie);
+console.log(document.cookie);
